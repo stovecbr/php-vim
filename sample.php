5557770a8c33e646617caf579ddf3fa40a2782b9
stovecbr <pc-018@st-ove.co.jp>
2023-02-06 12:49:54 +0900

    test

---
+++ sample.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>今日は何日？</title>
+  </head>
+  <body>
+    <h1>何月何日ですか？？？？</h1>
+    <p>
+      今日の日付は...
+      <?php
+      echo date('Y年m月d日');
+      ?>
+      ですよ。
+    </p>
+  </body>
+</html>
+
+
+
+
+
+
